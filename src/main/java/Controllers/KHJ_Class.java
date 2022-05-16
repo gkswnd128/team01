435c66cddad75ef4293195c50e87b087a76a3229
KHJ <kkran94@naver.com>
2022-05-16 12:43:30 +0900

    add mgs

--- src/main/java/Controllers/KHJ_Class.java
+++ src/main/java/Controllers/KHJ_Class.java
@@ -1,5 +1,14 @@
 package Controllers;
 
+import java.util.Scanner;
+
 public class KHJ_Class {
 
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("내용 한번 추가해보기");
+
+	}
 }
